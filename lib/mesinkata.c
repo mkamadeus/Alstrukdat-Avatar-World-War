7a2a64748ba1e13b18dbda70779adc76e1585190
regnents <rafaelseanputra@gmail.com>
2019-10-26 05:12:37 +0700

    Add mesinkar and mesinkata

---
+++ lib/mesinkata.c
@@ -0,0 +1,78 @@
+/* NAMA : RAFAEL SEAN PUTRA */
+/* NIM : 13518119 */
+/* TANGGAL : 20 September 2019 */
+/* program mesinkata.c */
+
+# include "mesinkata.h"
+
+/* State Mesin Kata */
+boolean EndKata;
+Kata CKata;
+
+void IgnoreBlank()
+/* Mengabaikan satu atau beberapa BLANK
+   I.S. : CC sembarang
+   F.S. : CC ≠ BLANK atau CC = MARK */
+{
+    /* KAMUS LOKAL */
+
+    /* ALGORITMA */
+    while ((CC == BLANK) && (CC != MARK)) {
+        ADV();
+    }
+}
+
+void STARTKATA()
+/* I.S. : CC sembarang
+   F.S. : EndKata = true, dan CC = MARK;
+          atau EndKata = false, CKata adalah kata yang sudah diakuisisi,
+          CC karakter pertama sesudah karakter terakhir kata */
+{
+    START();
+    IgnoreBlank();
+    if (CC == MARK) {
+        EndKata = true;
+    }
+    else {
+        EndKata = false;
+        SalinKata();
+    }
+}
+
+void ADVKATA()
+/* I.S. : CC adalah karakter pertama kata yang akan diakuisisi
+   F.S. : CKata adalah kata terakhir yang sudah diakuisisi,
+          CC adalah karakter pertama dari kata berikutnya, mungkin MARK
+          Jika CC = MARK, EndKata = true.
+   Proses : Akuisisi kata menggunakan procedure SalinKata */
+{
+    IgnoreBlank();
+    if (CC == MARK) {
+        EndKata = true;
+    }
+    else {
+        SalinKata();
+        IgnoreBlank();
+    }
+}
+
+void SalinKata()
+/* Mengakuisisi kata, menyimpan dalam CKata
+   I.S. : CC adalah karakter pertama dari kata
+   F.S. : CKata berisi kata yang sudah diakuisisi;
+          CC = BLANK atau CC = MARK;
+          CC adalah karakter sesudah karakter terakhir yang diakuisisi.
+          Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
+{
+    /* KAMUS LOKAL*/
+    int i;
+
+    /* ALGORITMA */
+    i = 1;
+    while ((CC != MARK) && (CC != BLANK) && (i <= NMax)) {
+        CKata.TabKata[i] = CC;
+        ADV();
+        i++;
+    }
+    CKata.Length = i - 1;
+}
