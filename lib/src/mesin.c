ebcea7242c307eb192781227f4ef0499ae3bcb45
Filbert Wijaya <filbertwijaya@gmail.com>
2019-11-19 19:55:50 +0700

    reworking on mesin (hopefully it works)

--- lib/src/mesin.c
+++ lib/src/mesin.c
@@ -1,5 +1,4 @@
 # include "../include/mesin.h"
-# include "../include/array.h"
 # include <stdio.h>
 
 void IgnoreBlank()
@@ -17,55 +16,48 @@ int BacaAngka() {
     int n, temp;
     IgnoreBlank();
     while ((CC != EOF) && (CC != BLANK)) {
-        n = n * 10;
-        temp = CC - "";
-        n = n + temp;
+        n = n *10 + (int)CC;
         ADV();
     }
     IgnoreBlank();
     return n;
 }
 
-Point Peta ()
+matrix Peta (matrix *map)
 /* Fungsi Peta membaca dan mengirim ukuran peta permainan */
 {
-    int i, x, y, temp;
-    Point P;
+    int i, row, col, temp;
 
-    x = 0;
+    row = 0;
     while ((CC != EOF) && (CC != BLANK)) {
-        x = x * 10;
-        temp = CC - "";
-        x = x + temp;
+        row = row * 10 + (int)CC;
         ADV();
     }
     IgnoreBlank();
-    y = 0;
+    col = 0;
     while ((CC != EOF) && (CC != BLANK)) {
-        y = y * 10;
-        temp = CC - "";
-        y = y + temp;
+        col = col * 10 + (int)CC;
         ADV();
     }
-    P.X = x;
-    P.Y = y;
-    return P;
+    createEmpty(row,col,map);
+    return *map;
 }
 
 buildingCoord Bangunan()
 /* Fungsi mengirim daftar bangunan yang ingin dimasukkan ke dalam peta permainan */
 {
     buildings B;
-    int row, col, temp;
+    int row, col;
 
     IgnoreBlank();
-    if (CC == 'C') 
-    B = CC;
+    if (CC == 'C') makeCastle(&B,0);
+    else if (CC == 'F') makeFort(&B,0);
+    else if (CC == 'T') makeTower(&B,0);
+    else if (CC == 'V') makeVillage(&B,0);//kondisikan ownernya
     ADV();
     IgnoreBlank();
     row = 0;
     while ((CC != EOF) && (CC != BLANK)) {
-        temp = (int)CC;
         row = row * 10 + (int)CC;
         ADV();
     }
@@ -79,15 +71,15 @@ buildingCoord Bangunan()
 }
 
 int BacaFile () {
-    Point P;
+    matrix map;
     int n, i, j;
     buildingCoord B;
     TabInt BArr;
-    int M[IdxMax][IdxMax];
+    int M[IdxMax][IdxMax];//graph (?)
 
     START();
     IgnoreBlank();
-    P = Peta();
+    map = Peta(&map);
     IgnoreBlank();
     n = BacaAngka();
     makeEmptyArray(&BArr,n);
@@ -95,6 +87,7 @@ int BacaFile () {
     for (i = 1; i <= n; i++) {
         B = Bangunan();
         bacaIsi(&BArr,B);
+        insertStructure(&map,B);
     }
     IgnoreBlank();
     for (i = 1;i <= n; i++) {
